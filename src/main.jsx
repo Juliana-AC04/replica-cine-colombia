bf84eca24297ef029bdeb0af36cbae28aa0ab9dc
Juliana Argaez <argaez8533025@gmail.com>
2024-03-26 20:24:38 -0500

    Se crean carpetas y archivos de inicio

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Index from './pages/Index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Index/>
   </React.StrictMode>,
 )
---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import Carousel from "../components/Carousel";
+import Header from "../components/Header";
+import ListMovies from "../components/ListMovies";
+
+
+export default function Index() {
+  return (
+    <div>
+      <Header></Header>
+      <Carousel></Carousel>
+      <ListMovies></ListMovies>
+    </div>
+  )
+}
