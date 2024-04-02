6bb75c7e563777f457541a04bb8b2bb4c07b3716
Juliana Argaez <argaez8533025@gmail.com>
2024-04-01 22:45:33 -0500

    Co-authored-by: Estefania villan <Estefaniavillan@users.noreply.github.com> Co-authored-by: Gabriela Aguiar <GabrielaAguiarMarquez@users.noreply.github.com> Co-authored-by: Sofía Vitery <rojasofia@users.noreply.github.com>

--- src/router/IndexRouter.jsx
+++ src/router/IndexRouter.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import Index from '../pages/Index';
 import { Login } from '../pages/Login/Login';
-// import Schedule from '../components/Schedule';
-// import MovieDetails from '../components/MovieDetails';
 import { CompraForm } from '../components/FormCompra/CompraForm';
 import ResumenCompra from '../components/FormCompra/ResumenCompraForm';
 import Details from '../pages/Details/Details';
