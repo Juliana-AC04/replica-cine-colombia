06c04054675c06d6bdb81758c0caa78746e33fed
Juliana Argaez <argaez8533025@gmail.com>
2024-04-01 20:31:42 -0500

    Se crea componente de asientos pero sin json server

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,17 +2,19 @@ import Header from "../components/Header";
 import ListMovies from "../components/ListMovies";
 import MovieDetails from "../components/MovieDetails";
 import Schedule from "../components/Schedule";
+import Seats from "../components/Seats";
 
 
 export default function Index() {
   return (
     <div>
       <Header></Header>
+      <ListMovies></ListMovies>
       <div className="flex flex-col sm:flex-row">
         <MovieDetails></MovieDetails>
         <Schedule></Schedule>
       </div>
-      <ListMovies></ListMovies>
+      <Seats></Seats>
     </div>
   )
 }
